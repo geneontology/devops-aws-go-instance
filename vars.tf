64f54d70b7098b49cd445eeccaf5a9cd4a45a00c
Abdelilah Essiari <aessiari@lbl.gov>
2022-05-05 19:13:04 -0700

    enables pointing to existing vpc

--- vars.tf
+++ vars.tf
@@ -5,11 +5,13 @@ variable "tags" {
 variable vpc_id {
   type = string
   description = "ID of an existing VPC."
+  default = "vpc-0926e26ef0721bf97"
 }
 
 variable subnet_id {
   type = string
   description = "ID of an existing subnet"
+  default = "subnet-0a09e8ea837f8606b"
 }
 
 variable "region" {
