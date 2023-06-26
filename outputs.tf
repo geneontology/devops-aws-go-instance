ff8592499eb3c4dd5a1b4bf1d1ad424dd8b0a9fc
Abdelilah Essiari <aessiari@lbl.gov>
2023-06-26 13:16:25 -0700

    fixed output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "public_ip" {
-  value = "${var.use_elastic_ip == true ? aws_eip.go_instance_eip.public_ip : aws_instance.go_instance.public_ip}"
+  value = "${var.use_elastic_ip == true ? aws_eip.go_instance_eip[0].public_ip : aws_instance.go_instance.public_ip}"
 }
