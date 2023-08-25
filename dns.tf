344c11597397d18938bb515a522ac12ef1a9583a
Abdelilah Essiari <aessiari@lbl.gov>
2023-08-25 05:47:48 +0000

    allowing for a list of dns records

--- dns.tf
+++ dns.tf
@@ -1,8 +1,8 @@
 resource "aws_route53_record" "dns_record" {
-    zone_id = var.dns_zone_id
-    name    = var.dns_record_name
+    for_each = local.dns_record_name
+    zone_id = "${var.dns_zone_id != "" ? var.dns_zone_id : "REPLACE_ME"}"
+    name    = each.value
     type    = "A"
     ttl     = "300"
     records = ["${var.use_elastic_ip == true ? aws_eip.go_instance_eip[0].public_ip : aws_instance.go_instance.public_ip}"]
-    count = "${var.dns_record_name != "" ? 1 : 0}"
 }
